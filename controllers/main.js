6cb1cab87518c0b7101443bdbac0a398f6afad0d
liqiang <xlqdys@foxmail.com>
2019-05-13 17:55:14 +0800

    使用数据库，添加mysql连接池

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,7 @@
+const query=require('../db')
 let fn_home = async (ctx, next) => {
+  let data=await query('SELECT * FROM demo')
+  console.log(data)
   ctx.response.body = '<h1>hello</h1>'
 }
 module.exports={
